7d7c27f8b09931f21e3c1e3196760d9262621262
Anitas-Vlad <anitas.vlad123@gmail.com>
2023-06-13 23:52:00 +0300

    changed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,16 +6,26 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="bg-light mx-8 rounded-2xl p-4">
-      <h1 className="text-dark-apple text-center text-xl font-normal">
+    <div className="border-2 border-light mx-8 rounded-2xl p-4">
+      <h1 className="text-light text-center text-xl font-normal">
         Hey, my name is Vlad
       </h1>
       <br></br>
-      <h1 className="text-dark-apple text-center text-xl font-normal">
+      <h1 className="text-light text-center text-xl font-normal">
         I will be your next junior developer!
       </h1>
     </div>
   );
 }
+
+{/* <div className="border-2 border-light mx-8 rounded-2xl p-4">
+  <h1 className="text-light text-center text-xl font-normal">
+    Hey, my name is Vlad
+  </h1>
+  <br></br>
+  <h1 className="text-light text-center text-xl font-normal">
+    I will be your next junior developer!
+  </h1>
+</div> */}
 
 export default Header;
