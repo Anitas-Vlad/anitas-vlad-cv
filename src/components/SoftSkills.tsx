f74dcd85b2424544af6aae89ace63af9d6506a89
Anitas-Vlad <anitas.vlad123@gmail.com>
2023-06-14 00:14:14 +0300

    added shadows

--- src/components/SoftSkills.tsx
+++ src/components/SoftSkills.tsx
@@ -3,10 +3,10 @@ import React from "react";
 function SoftSkills(){
     return (
         <div className="flex flex-col mt-6">
-            <div className="flex relative bg-light w-28 h-28 rounded-full justify-center items-center top-14 self-center">
+            <div className="flex relative bg-light w-28 h-28 rounded-full justify-center items-center top-14 self-center z-10">
                 <p className=" text-dark-apple font-medium p-3">Soft Skills</p>
             </div>
-            <div className="mx-8 rounded-xl bg-light bg-opacity-20 px-8 pt-20 pb-16 border-2 border-light">
+            <div className="mx-8 rounded-xl bg-light bg-opacity-20 px-8 pt-20 pb-16 border-2 border-light drop-shadow-md z-0">
                 <p className="text-light font-medium text-center">
                     &#9; Communication, Empathy, Open-mindedness, Creativity, Teamwork and Collaboration
                 </p>
