c591ccd13d5455294e91bcb9626d17945754ee0a
Anitas-Vlad <anitas.vlad123@gmail.com>
2023-06-14 00:19:38 +0300

    removed redundant block

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,14 +18,4 @@ function Header() {
   );
 }
 
-{/* <div className="border-2 border-light mx-8 rounded-2xl p-4">
-  <h1 className="text-light text-center text-xl font-normal">
-    Hey, my name is Vlad
-  </h1>
-  <br></br>
-  <h1 className="text-light text-center text-xl font-normal">
-    I will be your next junior developer!
-  </h1>
-</div> */}
-
 export default Header;
