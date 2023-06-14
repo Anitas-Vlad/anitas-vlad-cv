f12903ada57738eb2416b48da45b0a48ac733c10
Anitas-Vlad <anitas.vlad123@gmail.com>
2023-06-14 17:22:32 +0300

    fixed contact href to github

--- src/components/ContactAndLinks.tsx
+++ src/components/ContactAndLinks.tsx
@@ -16,7 +16,7 @@ function ContactAndLinks(){
                         <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
                     </svg>
                 </a>
-                <a className="flex content-center mx-4 justify-center text-center p-2 border-2 border-light rounded-xl w-10" href="https://www.linkedin.com/in/vlad-anitas-388a21223/">
+                <a className="flex content-center mx-4 justify-center text-center p-2 border-2 border-light rounded-xl w-10" href="https://github.com/Anitas-Vlad">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         className="h-5 w-5 text-light"
